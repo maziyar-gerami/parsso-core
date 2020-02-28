5fc721510b13caaec4ebd314943de0b80809a769
H. Monkaresi <monkaresi@gmail.com>
2020-02-28 12:09:25 +0330

    Added JSON service registry

--- parsso-install.sh
+++ parsso-install.sh
@@ -3,8 +3,11 @@ systemctl stop tomcat
 rm -rf /opt/workspace/cas-overlay-template/build
 ./gradlew build
 rm -rf /etc/cas/config
+rm -rf /etc/cas/services
+
 cp -i /opt/workspace/cas-overlay-template/build/libs/cas.war /opt/tomcat/latest/webapps/
 cp -r /opt/workspace/cas-overlay-template/etc/cas/config/ /etc/cas/
+cp -r /opt/workspace/cas-overlay-template/etc/cas/services /etc/cas/
 chown root:tomcat /opt/tomcat/latest/webapps/cas.war
 chown root:tomcat /etc/cas/config/
 chmod g-w,o-rwx /opt/tomcat/latest/webapps/cas.war
