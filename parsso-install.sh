7f422e93c88da382b23046d25c10615926d69cfe
H. Monkaresi <monkaresi@gmail.com>
2020-03-14 10:58:58 +0330

    1 and 2. Added Active Directory authentication 3. Modified log setting 4. Modified parsso install shell

--- parsso-install.sh
+++ parsso-install.sh
@@ -1,17 +1,26 @@
+
 echo "--- Installing on `hostname`"
+
 systemctl stop tomcat
 rm -rf /opt/workspace/cas-overlay-template/build
-./gradlew build
 rm -rf /etc/cas/config
 rm -rf /etc/cas/services
 
+./gradlew build
+
+cd /opt/tomcat/latest/
+rm -rf webapps/cas work/Catalina/localhost/cas
+cd /
+
 cp -i /opt/workspace/cas-overlay-template/build/libs/cas.war /opt/tomcat/latest/webapps/
-cp -r /opt/workspace/cas-overlay-template/etc/cas/config/ /etc/cas/
+cp -r /opt/workspace/cas-overlay-template/etc/cas/config /etc/cas/
 cp -r /opt/workspace/cas-overlay-template/etc/cas/services /etc/cas/
+
 chown root:tomcat /opt/tomcat/latest/webapps/cas.war
 chown root:tomcat /etc/cas/config/
 chmod g-w,o-rwx /opt/tomcat/latest/webapps/cas.war
 chmod g-w,o-rwx /etc/cas/config/
+
 systemctl start tomcat
 echo "Installation complete."
 
